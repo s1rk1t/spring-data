ecca796d1fe69ea15d56dde4364b412293f09e04
s1rk1t <37907871+s1rk1t@users.noreply.github.com>
2022-01-24 22:37:36 -0500

    Spring Data REST CRUD app

---
+++ spring-data-rest-crud-demo/src/main/java/com/luv2code/springboot/cruddemo/entity/Employee.java
@@ -0,0 +1,41 @@
+package com.luv2code.springboot.cruddemo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "employee")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+	public Employee(String firstName, String lastName, String email) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+	@Column(name = "first_name")
+	private String firstName;
+
+	@Column(name = "last_name")
+	private String lastName;
+
+	@Column(name = "email")
+	private String email;
+
+}
